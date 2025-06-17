2e34f9a1f8cad5f4493fe8a0ae218caad932f302
Anshit Kumar <85104688+AKM257@users.noreply.github.com>
2025-06-17 22:03:42 +0530

    Delete stock_price.py

--- stock_price.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-import yfinance as yf
-end = datetime.now()
-start = datetime(year = end.year-20,month = end.month,day = end.day)
-stock = "GOOG"
-google_data = yf.download(stock,start,end)
-
-
